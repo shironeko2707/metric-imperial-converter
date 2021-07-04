4fa4850872eb1cf10a67712b46fa53a145fe8079
Glitch (troubled-reminiscent-vanadium) <none>
2021-07-04 02:13:09 +0000

    🦁🚙 Checkpoint ./controllers/convertHandler.js:32307034/671

--- controllers/convertHandler.js
+++ controllers/convertHandler.js
@@ -1,11 +1,21 @@
 function ConvertHandler() {
   
   this.getNum = function(input) {
-    let result;
+    let strRegEx = /\.?[0-9]+(\.[0-9]+?)?(\/\.?[0-9]+(\.[0-9]+?)?)?/g;
+    let result = input.match(strRegEx);
     
-    return result;
+    if( result === null){
+      return 1;
+    }
+    if (result.length > 1 ){
+      return {"error" : "invalid number"}
+    }
+  const divideArr = result[0].split('/');
+    if(divideArr.length === 2){
+      return parseFloat(divideArr[0]) / parseFloat(divideArr[1])
+    }
+    return result[0] != undefined? parseFloat(result[0]): { "error": "invalid number"};
   };
-  
   this.getUnit = function(input) {
     let result;
     
