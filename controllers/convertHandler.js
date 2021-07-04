3974f3b5dfd00debd956b44f7622b353b4fdac0d
Glitch (troubled-reminiscent-vanadium) <none>
2021-07-04 03:45:09 +0000

    🌎🐪 Checkpoint ./package.json:32307034/3893 ./sample.env:32307034/6 ./controllers/convertHandler.js:32307034/15058 ./routes/api.js:32307034/2

--- controllers/convertHandler.js
+++ controllers/convertHandler.js
@@ -1,42 +1,43 @@
-function ConvertHandler() {
-  
+function ConvertHandler() {  
+  //for decimal
   this.getNum = function(input) {
-    let strRegEx = /\.?[0-9]+(\.[0-9]+?)?(\/\.?[0-9]+(\.[0-9]+?)?)?/g;
+    let strRegEx = /\.?[0-9]+(\.[0-9]+?)?(\/\.?[0-9]+(\.[0-9]+?)?)?/g;    
     let result = input.match(strRegEx);
-    
-    if( result === null){
+
+    if(result === null){
       return 1;
     }
-    if (result.length > 1 ){
-      return {"error" : "invalid number"}
+    
+    if(result.length > 1){    
+      return { "error": "invalid number" } 
     }
-  const divideArr = result[0].split('/');
+
+    const divideArr = result[0].split('/');
     if(divideArr.length === 2){
       return parseFloat(divideArr[0]) / parseFloat(divideArr[1])
     }
     return result[0] != undefined? parseFloat(result[0]): { "error": "invalid number"};
   };
+  
   this.getUnit = function(input) {
     let strRegEx = /[a-z]+/ig;
     let result = input.match(strRegEx);
-    if (result === null){
-      return {"error":"no unit given"}
-    }
-   return result[0];
+    if(result === null){
+      return {"error": "no unit given"}
+    }    
+    return result[0];   
   };
   
   this.getReturnUnit = function(initUnit) {
     const CONVERT_TO = {
-      gal : 'L',
-      L : 'gal',
-      mi : 'km',
-      km : 'mi',
-      lbs :'kg',
-      kg :'lbs'
-      
+      km: 'mi',
+      mi: 'km',
+      l: 'gal',
+      gal: 'L',
+      kg: 'lbs',
+      lbs: 'kg'
     };
-    return (CONVERT_TO[initUnit.toLowerCase()] === undefined) ? {"error":"invalid unit"} : CONVERT_TO[initUnit.toLowerCase()];
-   
+    return (CONVERT_TO[initUnit.toLowerCase()] === undefined) ? { "error": "invalid unit"} : CONVERT_TO[initUnit.toLowerCase()];
   };
 
   this.spellOutUnit = function(unit) {
